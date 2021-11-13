3ee9bce1e11a662b2c47859e951763792b105314
marcelaeyh <marcelaecheverrigallego@gmail.com>
2021-11-13 00:03:52 -0500

    Resolucion de problemas

--- htmlFile.js
+++ htmlFile.js
@@ -1,5 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+/* Aquí va la ruta a la carpeta frontend */
 connect().use(serveStatic("/Users/mac/git/frontend/")).listen(3000, function(){
     console.log('El servidor está corriendo en el puerto 3000');
 });
