06abef5f4db8250c52c415fd834ff3faa0e15fbb
Your Name <danilocm2610>
2021-12-02 19:07:39 -0500

    hol

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+var location = require('jquery');
+const bodyParser = require('body-parser')
+const app = express();
+const bcrypt= require('bcrypt');
+const mongoose=require('mongoose');
+const user = require('./public/user.js');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+const mongo_uri ='mongodb://localhost:27017/tienda'
+mongoose.connect(mongo_uri,function(err){
+    if(err){
+        console.log('Inicia Log de errores de Mongo');
+        throw  err;
+        console.log('Finaliza Log de errores de Mongo');
+    }
+    else{
+        console.log('se conectó a ${mongo_uri}');
+    }
+});
+
+app.post('/register',(req,res) => { 
+    
+    const {username,password} =req.body;
+    console.log('Entra a metodo post/register');
+const  User=new user({username,password});
+console.log('Declara variable user');
+    User.save(err =>{
+        if(err){
+            res.status(500).send('Error al registrar usuario');
+        }
+        else{
+            res.status(200).send('Usuario Registrado');
+        }
+    });
+    
+});
+app.post('/authenticate',(req,res) => { 
+    console.log('verificar');
+    const {username,password} =req.body;
+    user.findOne({username},(err,user) => {
+        if(err){
+            res.status(500).send('Error al autenticar usuario');
+        }else if(!user){
+            res.status(500).send('El usuario no existe');
+        } else
+        {
+            user.isCorrectPassword(password,(err,result)=>{
+                if(err){
+                    res.status(500).send('Error al autenticar');
+                }else if(result){
+                    if (typeof window !== "undefined") {
+                        window.location.href='clientes.html';   
+                      }
+                }else{
+                    res.status(500).send('usuario o clave incorrecta');
+                }
+            });
+        }
+    });
+});
+app.listen(3000,() => {
+        console.log('Servidor Inicia puerto 3000');
+})
+module.exports=app;
--- htmlFile.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-/* Aquí va la ruta a la carpeta frontend */
-connect().use(serveStatic("/Users/mac/git/frontend/")).listen(3000, function(){
-    console.log('El servidor está corriendo en el puerto 3000');
-});
